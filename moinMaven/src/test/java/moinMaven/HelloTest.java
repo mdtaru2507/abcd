3c1761ab3cb89c876419dec83165ef856ea9a9e7
MD MOIN UDDIN <manhamahdi@mds-macbook-pro.mynetworksettings.com>
2023-10-15 23:36:18 -0400

    add new method and veriable

--- moinMaven/src/test/java/moinMaven/HelloTest.java
+++ moinMaven/src/test/java/moinMaven/HelloTest.java
@@ -1,12 +1,17 @@
 package moinMaven;
 
 public class HelloTest {
-	
-	
-public void git76() {
-		
+
+	public void git76() {
+
 		System.out.println("manhamahdi");
 	}
 
+	int a = 3445;
+
+	public void git176() {
+
+		System.out.println("manhamahdi");
+	}
 
 }
